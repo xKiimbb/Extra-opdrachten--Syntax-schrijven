7dd689f9ea81537ff18b58d296e535b53f6725a4
xKiimbb <xkiimbbmusic@gmail.com>
2024-10-16 21:01:44 +0200

    Opdracht-2-Opgelost

---
+++ Opdracht-2.js
@@ -0,0 +1,9 @@
+// Definieer een functie genaamd drieGetallenOptellen.
+// De functie accepteert drie parameters, genaamd x, y en z.
+// Geef de optelsom van de drie parameters terug als resultaat.
+
+function drieGetallenOptellen(x, y, z) {
+  return x + y + z;
+}
+
+console.log(drieGetallenOptellen(1, 2, 3));
